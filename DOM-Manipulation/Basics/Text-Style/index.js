d04f732c251638404bf8aa450d351022e0e63769
Roland-Sankara <roland1sankara@gmail.com>
2025-01-09 11:38:34 +0300

    add text style project

---
+++ DOM-Manipulation/Basics/Text-Style/index.js
@@ -0,0 +1,37 @@
+// import the DOM nodes
+let heroSection = document.querySelector('#hero__section');
+let colorMenu = document.querySelector("#colors");
+let bgColorMenu = document.querySelector("#bg-colors");
+let fontMenu = document.querySelector("#fonts");
+let fontSizeMenu = document.querySelector("#font-size");
+
+// function to update color
+const updateColor = (event)=>{
+    let color = event.target.value;
+    heroSection.style.color = color;
+}
+
+// function to update bg-color
+const updateBGColor = (event)=>{
+    let bgColor = event.target.value;
+    heroSection.style.backgroundColor = bgColor;
+}
+
+// function to update font size
+const updateFontSize = (event)=>{
+    let fontSize = event.target.value;
+    heroSection.style.fontSize = fontSize;
+}
+
+// function to update font family
+const updateFontFamily = (event)=>{
+    let font = event.target.value;
+    heroSection.style.fontFamily = font;
+}
+
+
+// Add EventListeners to each the menus
+colorMenu.addEventListener('change', (e)=>updateColor(e));
+bgColorMenu.addEventListener('change', (e)=>updateBGColor(e));
+fontMenu.addEventListener('change', (e)=>updateFontFamily(e));
+fontSizeMenu.addEventListener('change', (e)=>updateFontSize(e));
